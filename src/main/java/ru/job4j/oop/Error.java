d8fd9e1fe3f01a01057f8cedfb2d9bcf8dd8d157
Vitaliy <vivasiliev@icloud.com>
2023-06-25 17:35:36 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Error {
+    private  boolean active;
+    private int status;
+    private String message;
+
+    public Error() { }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printinfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 2, "Test");
+        error.printinfo();
+    }
+}
