76413224fd9ffd08e3d949c1e25f407239de556c
Vitaliy <vivasiliev@icloud.com>
2023-06-26 18:15:08 +0300

    Правила Checkstyle [#504981 #424814]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,10 +1,7 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public static final String newValue() {
-     return "";
-    }
-
+    public static final String NEW_VALUE = "";
     private String name;
     private String surname;
     private int sizeOfEmpty = 10;
