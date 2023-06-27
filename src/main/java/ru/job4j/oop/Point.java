6880c6f5d8026d5a677b489ce8b55122d97fe492
Vitaliy <vivasiliev@icloud.com>
2023-06-27 13:00:31 +0300

    Рефакторинг - Площадь треугольника. [#122642]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -19,7 +19,7 @@ public class Point {
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
+        Point b = new Point(4, 0);
         double dist = a.distance(b);
         System.out.println(dist);
     }
