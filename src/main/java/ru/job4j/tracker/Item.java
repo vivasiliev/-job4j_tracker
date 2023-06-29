641060f346fdbafa65a77fd8a243c76740fa220b
Vitaliy <vivasiliev@icloud.com>
2023-06-29 20:37:09 +0300

    Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() { }
 
@@ -13,6 +16,10 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
@@ -30,4 +37,5 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
 }
